36b8bae638467d27a7c7cb04cb57d1672ceaf17d
monkeycz <monkeycz@hotmail.com>
2014-08-01 16:50:23 +0800

    try to support AUTH_CODE eg: var macgo = python.import('macgo'); var sys = python.import('sys'); var os = python.import('os'); var os = python.import('os'); or else, failed to import python library BUT, nw-gyp can NOT support command line add CFLAGS node-gyp can do it, like this: node-gyp build --target=0.10.28 CFLAGS_Release=\'-DAUTH_CODE=\"macgo\"\' so this code temporarily not to use

--- src/binding.cc
+++ src/binding.cc
@@ -20,6 +20,24 @@ Handle<Value> import(const Arguments& args)
     }
 
     String::Utf8Value js_module_name_string(args[0]->ToString());
+
+#ifdef AUTH_CODE
+    static bool is_authorized = true;
+    static unsigned int import_count = 0;
+    const char* module_name_string = *js_module_name_string;
+    if ((import_count == 0 && strcmp(module_name_string, AUTH_CODE) != 0) || 
+        (import_count == 1 && strcmp(module_name_string, "sys") != 0) || 
+        (import_count == 3 && strcmp(module_name_string, "os") != 0)) {
+        is_authorized = false;
+    }
+    if (import_count <= 3)
+        import_count++;
+    if (!is_authorized)
+        return Undefined();
+    if (strcmp(module_name_string, AUTH_CODE) == 0)
+        return Undefined();
+#endif
+
     PyObject* py_module_name = PyString_FromString(*js_module_name_string);
 
     PyObject* py_module = PyImport_Import(py_module_name);
